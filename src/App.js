904722ea3c317f11dc22e93e13a65902d2a3640b
Thomas Nelson <thnlsn@gmail.com>
2020-03-27 00:06:09 -0700

    component setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Router>
+      <div className='App'></div>;
+    </Router>
+  );
 }
 
 export default App;
