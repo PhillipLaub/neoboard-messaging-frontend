a2cb5e4e4073c0e117fd48e0e022810cca64f55b
Thomas Nelson <thnlsn@gmail.com>
2020-03-27 18:45:23 -0700

    attempting global styles

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
+import Navbar from './components/navbar/Navbar';
+
 function App() {
   return (
     <Router>
-      <div className='App'></div>;
+      <div className='App'>
+        <Navbar />
+      </div>
     </Router>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
-  return <div></div>;
+  return <div className='navbar'>asdasd</div>;
 };
 
 export default Navbar;
