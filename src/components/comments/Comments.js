9bfee51c9fce0c561c554b44f8cd6ec9b8c47ae5
thnlsn <52093425+thnlsn@users.noreply.github.com>
2020-04-01 17:03:32 -0700

    temp

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -2,18 +2,18 @@ import React from 'react';
 import CommentItem from './CommentItem';
 
 const Comments = ({ comments }) => {
-  return (
-    <div className='comments'>
-      {comments.map(commentItem => (
-        <CommentItem
-          username={commentItem.username}
-          image={commentItem.image}
-          comment={commentItem.comment}
-          key={commentItem.id}
-        />
-      ))}
-    </div>
-  );
+    return (
+        <div className='comments'>
+            {comments.map(commentItem => (
+                <CommentItem
+                    username={commentItem.username}
+                    image={commentItem.image}
+                    comment={commentItem.comment}
+                    key={commentItem.id}
+                />
+            ))}
+        </div>
+    );
 };
 
 export default Comments;
