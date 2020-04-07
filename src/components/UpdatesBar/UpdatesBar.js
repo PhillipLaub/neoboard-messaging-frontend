8fff5e7ab4b194c95a22861e8f46b39304608829
thnlsn <52093425+thnlsn@users.noreply.github.com>
2020-04-06 23:37:34 -0700

    adding section for time

--- src/components/UpdatesBar/UpdatesBar.js
+++ src/components/UpdatesBar/UpdatesBar.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './UpdatesBar.css';
 
 const UpdateBar = ({ subject }) => {
-  return <div className='update-bar'>{subject}</div>;
+    return (
+        <div className='update-bar'>
+            <div>{subject}</div>
+            <div>Time</div>
+        </div>
+    );
 };
 
 export default UpdateBar;
