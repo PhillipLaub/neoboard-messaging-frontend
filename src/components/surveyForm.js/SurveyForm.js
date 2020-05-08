716cc1780e6a107bf1c8b55d9e4a9f87d1c250d6
Thomas Nelson <thnlsn@gmail.com>
2020-05-08 05:03:38 -0700

    setting up folder structure and importing fragments

---
+++ src/components/surveyForm.js/SurveyForm.js
@@ -0,0 +1,45 @@
+import React, { Fragment } from 'react';
+import './SurveyForm.css';
+
+const SurveyForm = (
+  {
+    /*   privateUserInfo: { image, firstName, lastName, joinDate, college }, */
+  }
+) => {
+  return (
+    <div className='user-container'>
+      {/* This section is hard coded for now... */}
+      <div className='top-section-container'>
+        <img
+          className='user-page-image user-flex'
+          src={require(`../../${image}`)}
+        ></img>
+      </div>
+
+      <div className='basic-info'>
+        <div className='name center'>
+          {firstName} {lastName} <i class='fas fa-circle yellow no-shadow'></i>
+        </div>
+        <div className='basic-info-item top-margin-buffer'>
+          Overachieving since {joinDate}
+        </div>
+        <div className='basic-info-item'>{college}</div>
+      </div>
+
+      <div className='private-info'>
+        <div className='private-icon'>
+          <i class='fas fa-user-secret'></i>
+        </div>
+        <div className='private-message'>
+          {firstName} has requested that their profile remain private
+        </div>
+      </div>
+
+      <div className='return-footer'>
+        <div className='return-button'>Return</div>
+      </div>
+    </div>
+  );
+};
+
+export default SurveyForm;
