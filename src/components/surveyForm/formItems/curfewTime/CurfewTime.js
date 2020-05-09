55d8ab21534ab0c8155751bb6cd1d7dd0d727dcb
Thomas Nelson <thnlsn@gmail.com>
2020-05-08 23:43:54 -0700

    completed outer layer of clocks by calculating degrees

--- src/components/surveyForm/formItems/curfewTime/CurfewTime.js
+++ src/components/surveyForm/formItems/curfewTime/CurfewTime.js
@@ -5,7 +5,28 @@ const CurfewTime = () => {
   return (
     <div className='questionnaire-container'>
       <div className='questionnaire-title'>Curfew Time</div>
-      <div className='clock'></div>
+      <div className='half-container half-container-left'>
+        <div className='clock'>
+          <div class='time-item'>8</div>
+          <div class='time-item'>9</div>
+          <div class='time-item'>10</div>
+          <div class='time-item'>11</div>
+          <div class='time-item'>12</div>
+          <div class='time-item'>1</div>
+          <div class='time-item'>2</div>
+          <div class='time-item'>3</div>
+          <div class='time-item'>4</div>
+          <div class='center'>
+            <div class='time-item minute-item'>:45</div>
+            <div class='time-item minute-item'>:00</div>
+            <div class='time-item minute-item'>:15</div>
+            <div class='time-item minute-item'>:30</div>
+          </div>
+        </div>
+      </div>
+      <div className='half-container half-container-right'>
+        <div className='time-input'></div>
+      </div>
     </div>
   );
 };
