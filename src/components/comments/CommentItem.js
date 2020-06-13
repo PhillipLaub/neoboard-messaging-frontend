8f431fc10eababa955909d08ee5bdbf711cc4d82
Thomas Nelson <thnlsn@gmail.com>
2020-06-12 23:56:09 -0700

    sidebar initial component

--- src/components/comments/CommentItem.js
+++ src/components/comments/CommentItem.js
@@ -4,7 +4,7 @@ import './CommentItem.css';
 const Comment = ({ username, image, comment, timestamp, id }) => {
   return (
     <div className='comment'>
-      <div className='user-container'>
+      <div className='user-messaging-container'>
         <img className='image' src={require(`../../${image}`)}></img>
         <div className='username'>{username}</div>
         <div className='timestamp'>{timestamp}</div>
