e6ae77264080827aa14e6c3afac13675baf5c41c
Thomas Nelson <thnlsn@gmail.com>
2020-06-16 20:03:24 -0700

    finished basic box for questions

--- src/components/studyGroupQuestionnaire/StudyGroupQuestions.js
+++ src/components/studyGroupQuestionnaire/StudyGroupQuestions.js
@@ -4,20 +4,29 @@ import './StudyGroupQuestions.css';
 const StudyGroupQuestions = ({ comments, subject, newMessages }) => {
   return (
     <div className='study-group-questionnaire-container'>
-      <div className='question-box'>
+      {/* Box 1 */}
+      <div className='questionnaire-box questionnaire-top-border'>
         <div className='questionnaire-header'>Study Group Questionnaire</div>
-        <hr />
+        <hr className='questionnaire-break' />
         <div className='questionnnaire-description'>
           This questionnaire is for your online course. Please fill out this
           questionnaire to provide us some information on how you decide who to
           study with, all information collected will remain anonymous and will
           only be used to organize everyone into groups.
         </div>
-        <hr />
-        <div className='questionnnaire-question'>
+        <hr className='questionnaire-break' />
+        <div className='questionnaire-question'>
           Email address <span className='required'>*</span>
+          <input
+            type='text'
+            placeholder='Valid email address'
+            className='questionnaire-input'
+          ></input>
         </div>
       </div>
+
+      {/* Box 2 */}
+      <div className='questionnaire-box'></div>
     </div>
   );
 };
