cea5265eed88401caafa2182ac42b9ed6e114bd9
thnlsn <52093425+thnlsn@users.noreply.github.com>
2020-07-31 22:26:03 -0700

    fixed messaging bar margin

--- src/components/message/MessageBar.js
+++ src/components/message/MessageBar.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './MessageBar.css';
 
 const MessageBar = () => {
-  return (
-    <div className='message-bar'>
-      <form action='POST'>
-        <input type='text' className='input-message' name='message' />
-        <div className='buttons'>
-          <i class='fas fa-camera message-bar-button'></i>
-          <i class='fas fa-paperclip message-bar-button'></i>
-          <i class='far fa-paper-plane message-bar-button'></i>
+    return (
+        <div className='message-bar'>
+            <form action='POST' className='message-form'>
+                <input type='text' className='input-message' name='message' />
+                <div className='buttons'>
+                    <i class='fas fa-camera message-bar-button'></i>
+                    <i class='fas fa-paperclip message-bar-button'></i>
+                    <i class='far fa-paper-plane message-bar-button'></i>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default MessageBar;
